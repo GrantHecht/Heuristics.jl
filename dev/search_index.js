bb288fcd0cc8f9bf2d5a834c2a5ec70f4cb95b8e
Documenter.jl <documenter@juliadocs.github.io>
2021-06-23 01:51:05 +0000

    build based on b934f64

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Heuristics","category":"page"},{"location":"#Heuristics","page":"Home","title":"Heuristics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Heuristics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Heuristics]","category":"page"}]
+}
